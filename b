56f256565a667fd0a872d27196c46a8dafa56c9d
sangh <san.gh@clockwatching.net>
2009-10-20 04:36:08 -0400

    Added base files.

---
+++ b
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! echo 0 | bc -q -l >&/dev/null; then
+    if ! echo 0 | bc -l >&/dev/null; then
+        echo "\"bc -q -l\" or \"bc -l\" not found, not using any switches."
+		bc=bc
+    else
+        bc='bc -l'
+    fi
+else
+    bc='bc -l -q'
+fi
+
+
+if [ -z "$*" ]; then
+	$bc
+else
+	echo "$@" | $bc | strtr "\n" " = $*\n"
+fi
---
+++ see-colours.sh
@@ -0,0 +1,25 @@
+e () {
+echo -e "$@"
+}
+
+e
+e "Black  (30): \033[00;30mThis is it.\033[01;30m And bold.\033[00m"
+e "Red    (31): \033[00;31mThis is it.\033[01;31m And bold.\033[00m"
+e "Green  (32): \033[00;32mThis is it.\033[01;32m And bold.\033[00m"
+e "Yellow (33): \033[00;33mThis is it.\033[01;33m And bold.\033[00m"
+e "Blue   (34): \033[00;34mThis is it.\033[01;34m And bold.\033[00m"
+e "Magenta(35): \033[00;35mThis is it.\033[01;35m And bold.\033[00m"
+e "Cyan   (36): \033[00;36mThis is it.\033[01;36m And bold.\033[00m"
+e "White  (37): \033[00;37mThis is it.\033[01;37m And bold.\033[00m"
+e
+e
+e "Background\033[00;30m(Colour Example)\033[00m(Norm: Black ) is  47  79  79"
+e "Foreground\033[00;37m(Colour Example)\033[00m(Norm: White ) is 245 222 179"
+e "Ref-Prepro\033[00;35m(Colour Example)\033[00m(Norm:Magenta) is 127 205 212"
+e "Type Mod. \033[00;32m(Colour Example)\033[00m(Norm: Green ) is 152 251 152"
+e "Keyword   \033[00;33m(Colour Example)\033[00m(Norm: Yellow) is 176 196 222"
+e "Const/Str \033[00;31m(Colour Example)\033[00m(Norm: Red   ) is 255 160 122"
+e "Comment   \033[00;34m(Colour Example)\033[00m(Norm: Blue  ) is 255 127 80"
+
+
+e
