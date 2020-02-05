78c8cf06eca7540cb2f0ed1c7fa77521cc7486e1
San GH <san.gh@clockwatching.net>
2020-02-05 12:24:19 -0800

    Icdiff now ships with a program called git-icdiff that is better than mine.

--- icdiff-git-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# The `diff` program is called by git with 7 parameters:
-#   path old-file old-hex old-mode new-file new-hex new-mode
-# We don't care about any of them except the file names.
-# We `cat` b/c `icdiff` is interactive by default, and the term can do that.
-exec icdiff "$2" "$5" | cat
